76ababf771ff5fd498bf2e5c1839141d95b5a7f3
gidoichi <hakuto1yds@gmail.com>
2025-04-06 10:12:02 +0900

    server.py returns values from tp401t

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
 import logging
+import os
+import sys
 
 import bme280
 import flask
 import smbus2
+
+sys.path.append("shellmag57")
+
+from shellmag57.TP401T import TP401T
+from shellmag57.VCNL4020 import VCNL4020
 
 BME280_ADDR = 0x76
 BUS_NO = 1
@@ -20,6 +27,7 @@ werkzeug_logger.setLevel(logging.WARNING)
 def sample():
     result = {
         "bme280": sample_bme280(),
+        "tp401t": sample_tp401t(),
         "vcnl4020": sample_vcnl4020(),
     }
     return result
@@ -28,6 +36,7 @@ def sample():
 def sample_bme280():
     data = bme280.sample(i2c, BME280_ADDR)
     result = {
+        "enable": True,
         "temperature": {
             "value": data.temperature,
             "unit": "°C",
@@ -43,11 +52,30 @@ def sample_bme280():
     }
     return result
 
+@app.route("/tp401t")
+def sample_tp401t():
+    if os.path.exists(TP401T.SYSFS_PATH):
+        sensor = TP401T()
+        data = sensor.getVoltage(sensor.tp401_ch)
+        result = {
+            "enable": True,
+            "odor": {
+                "value": data,
+            },
+        }
+        return result
+
+    else:
+        return {
+            "enable": False,
+            "message": f"not found: {TP401T.SYSFS_PATH}",
+        }
+
 @app.route("/vcnl4020")
 def sample_vcnl4020():
-    from shellmag57.VCNL4020 import VCNL4020
     sensor = VCNL4020()
     result = {
+        "enable": True,
         "proximity": {
             "value": sensor.proximity,
         },
